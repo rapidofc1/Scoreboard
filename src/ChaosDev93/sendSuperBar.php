644ae0410a5ffc8642a2a87c25b2f043a137fee1
chaosfreak93 <36674649+chaosfreak93@users.noreply.github.com>
2018-08-11 08:45:54 +0000

    Addet Time to Scoreboard

--- src/ChaosDev93/sendSuperBar.php
+++ src/ChaosDev93/sendSuperBar.php
@@ -18,11 +18,13 @@ class sendSuperBar extends Task
 
     public function onRun($tick)
     {
-    	
         $all = $this->plugin->getServer()->getOnlinePlayers();
         $test = count($this->plugin->getServer()->getOnlinePlayers());
         $motd = $this->plugin->getServer()->getMotd();
         $max = $this->plugin->getServer()->getMaxPlayers();
+        $h = $this->plugin->getHour();
+        $m = $this->plugin->getMin();
+        $s = $this->plugin->getSek();
         $t = str_repeat(" ", 60);
         $anni = $this->plugin->anni;
         
@@ -43,13 +45,18 @@ class sendSuperBar extends Task
         }
         
         foreach ($all as $player) {
+        	$name = $player->getName();
+        	$this->config = $this->plugin->getConfig();
+            $this->config = new Config("/home/Datenbank/PurePerms/Spieler/" . strtolower($name) . ".yml", Config::YAML);
             if ($anni === 1) {
             	
             	$player->sendTip(
                                                "\n\n\n\n\n\n\n\n\n\n\n\n\n\n" . 
                                                $t . Color::BOLD . $motd . Color::RESET . "\n" .
                                                $t . Color::BOLD . "Name" . Color::GRAY . ": " . Color::RESET . Color::BLUE . $player->getName() . Color::RESET . "\n" . 
-                                               $t . Color::BOLD . "Spieler" . Color::GRAY . ": " . Color::RESET . Color::GREEN . $test . Color::GRAY . " / " . Color::RED . $max . Color::RESET . "\n" .
+                                               $t . Color::BOLD . "Rang" . Color::GRAY . ": " . Color::RESET . Color::DARK_BLUE . $this->config->get("group") . Color::RESET . "\n" . 
+                                               $t . Color::BOLD . "Spieler" . Color::GRAY . ": " . Color::RESET . Color::GREEN . $test . Color::GRAY . " / " . Color::RED . $max . Color::RESET . "\n" . 
+                                               $t . Color::BOLD . "Zeit" . Color::GRAY . ": " . Color::RESET . Color::YELLOW . $h . ":" . $m . ":" . $s . Color::RESET . "\n" . 
                                                $t . Color::BOLD . "Ping" . Color::GRAY . ": " . Color::RESET . Color::YELLOW . $player->getPing() . Color::RESET . "\n" . str_repeat("\n", 24));
             	
             } else if ($anni === 2) {
@@ -58,7 +65,9 @@ class sendSuperBar extends Task
                                                "\n\n\n\n\n\n\n\n\n\n\n\n\n\n" . 
                                                $t . Color::BOLD . $motd . Color::RESET . "\n" . 
                                                $t . Color::BOLD . "Name" . Color::GRAY . ": " . Color::RESET . Color::BLUE . $player->getName() . Color::RESET . "\n" .
+                                               $t . Color::BOLD . "Rang" . Color::GRAY . ": " . Color::RESET . Color::DARK_BLUE . $this->config->get("group") . Color::RESET . "\n" . 
                                                $t . Color::BOLD . "Spieler" . Color::GRAY . ": " . Color::RESET . Color::GREEN . $test . Color::GRAY . " / " . Color::RED . $max . Color::RESET . "\n" .
+                                               $t . Color::BOLD . "Zeit" . Color::GRAY . ": " . Color::RESET . Color::YELLOW . $h . ":" . $m . ":" . $s . Color::RESET . "\n" . 
                                                $t . Color::BOLD . "Ping" . Color::GRAY . ": " . Color::RESET . Color::YELLOW . $player->getPing() . Color::RESET . "\n" . str_repeat("\n", 24));
             	
             } else if ($anni === 3) {
@@ -67,7 +76,9 @@ class sendSuperBar extends Task
                                                "\n\n\n\n\n\n\n\n\n\n\n\n\n\n" . 
                                                $t . Color::BOLD . $motd . Color::RESET . "\n" . 
                                                $t . Color::BOLD . "Name" . Color::GRAY . ": " . Color::RESET . Color::BLUE . $player->getName() . Color::RESET . "\n" .
+                                               $t . Color::BOLD . "Rang" . Color::GRAY . ": " . Color::RESET . Color::DARK_BLUE . $this->config->get("group") . Color::RESET . "\n" . 
                                                $t . Color::BOLD . "Spieler" . Color::GRAY . ": " . Color::RESET . Color::GREEN . $test . Color::GRAY . " / " . Color::RED . $max . Color::RESET . "\n" .
+                                               $t . Color::BOLD . "Zeit" . Color::GRAY . ": " . Color::RESET . Color::YELLOW . $h . ":" . $m . ":" . $s . Color::RESET . "\n" . 
                                                $t . Color::BOLD . "Ping" . Color::GRAY . ": " . Color::RESET . Color::YELLOW . $player->getPing() . Color::RESET . "\n" . str_repeat("\n", 24));
             	
             }
