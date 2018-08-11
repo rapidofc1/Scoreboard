6e9a293eacf8c848821c7ed83473900eebc5ebd0
chaosfreak93 <36674649+chaosfreak93@users.noreply.github.com>
2018-08-11 08:47:59 +0000

    Addet Time to Scoreboard.php

--- src/ChaosDev93/Scoreboard.php
+++ src/ChaosDev93/Scoreboard.php
@@ -27,4 +27,17 @@ class Scoreboard extends PluginBase implements Listener {
         $this->getLogger()->info($this->prefix . Color::AQUA . "Made By" . Color::RED . " ChaosDev93!");
 		
     }
+
+public function getHour() {
+  $hour = strftime("%H", time())+$this->timeZone;
+  return $hour;
+}
+public function getMin() {
+	$min = strftime("%M", time())+$this->timeZone;
+	return $min;
+	}
+public function getSek() {
+	$sek = strftime("%S", time())+$this->timeZone;
+	return $sek;
+	}
 }
